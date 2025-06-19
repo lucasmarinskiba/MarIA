37995a6552256a15c7bd061145bbd0875a48bb96
Lucas Daniel Marín <116664832+lucasmarinskiba@users.noreply.github.com>
2025-06-19 16:36:06 -0300

    Create Level.cpp

---
+++ src/core/Level.cpp
@@ -0,0 +1,15 @@
+#include "Level.h"
+
+Level::Level(ResourceManager& rm) {
+    background.setTexture(rm.loadTexture("assets/textures/background.png"));
+}
+
+void Level::update(float, Player&, std::vector<NPC>&) {
+    // Lógica de nivel si hace falta
+}
+
+void Level::draw(sf::RenderWindow& window, Player& player, std::vector<NPC>& npcs) {
+    window.draw(background);
+    for (auto& npc : npcs) npc.draw(window);
+    player.draw(window);
+}
