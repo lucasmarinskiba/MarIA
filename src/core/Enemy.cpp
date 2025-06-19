ed0598921f946baf70a6ec2142a4e554787ac974
Lucas Daniel Marín <116664832+lucasmarinskiba@users.noreply.github.com>
2025-06-19 17:54:25 -0300

    Create Enemy.cpp

---
+++ src/core/Enemy.cpp
@@ -0,0 +1,24 @@
+#include "Enemy.h"
+
+Enemy::Enemy() {}
+
+void Enemy::init(const sf::Texture& tex, const sf::Vector2f& pos) {
+    setTexture(tex, 64, 64, 4, 0.13f);
+    setPosition(pos);
+    alive = true;
+    health = 30.f;
+}
+
+void Enemy::save(std::ofstream& out) const {
+    auto pos = getPosition();
+    out << pos.x << ' ' << pos.y << ' ' << health << ' ' << alive << '\n';
+}
+
+void Enemy::load(std::ifstream& in, const sf::Texture& tex) {
+    float x, y, h; int a;
+    in >> x >> y >> h >> a;
+    setTexture(tex, 64, 64, 4, 0.13f);
+    setPosition({x, y});
+    health = h;
+    alive = a;
+}
