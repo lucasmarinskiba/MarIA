0e74a9536f418ba637fe30838ddd25ceace3127d
Lucas Daniel Marín <116664832+lucasmarinskiba@users.noreply.github.com>
2025-06-19 17:32:36 -0300

    Update AnimatedSprite.h

--- src/core/AnimatedSprite.h
+++ src/core/AnimatedSprite.h
@@ -7,11 +7,13 @@ public:
     void setTexture(const sf::Texture& texture, int frameWidth, int frameHeight, int frameCount, float frameTime);
     void setPosition(const sf::Vector2f& p);
     void update(float dt);
+
+    sf::Vector2f getPosition() const;
 protected:
     virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 private:
     sf::Sprite sprite;
-    int frameWidth, frameHeight, frameCount;
+    int frameWidth = 0, frameHeight = 0, frameCount = 1;
     int currentFrame = 0;
-    float frameTime, timeSinceLast = 0.f;
+    float frameTime = 0.1f, timeSinceLast = 0.f;
 };
