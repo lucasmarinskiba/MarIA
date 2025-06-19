8176bebedc3314baba74b3fe72b047e5f8f996ed
Lucas Daniel Marín <116664832+lucasmarinskiba@users.noreply.github.com>
2025-06-19 17:40:21 -0300

    Update SaveSystem.h

--- src/core/SaveSystem.h
+++ src/core/SaveSystem.h
@@ -1,9 +1,10 @@
 #pragma once
 #include <string>
 #include "core/Player.h"
+#include "core/QuestSystem.h"
 
 class SaveSystem {
 public:
-    static void save(const Player& player, const std::string& file = "save.txt");
-    static bool load(Player& player, const std::string& file = "save.txt");
+    static void save(const Player& player, const QuestSystem& quests, const std::string& file = "save.txt");
+    static bool load(Player& player, QuestSystem& quests, const std::string& file = "save.txt");
 };
