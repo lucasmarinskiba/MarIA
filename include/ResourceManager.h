87260f395111093492f8d37edff63b3d16fd1942
Lucas Daniel Marín <116664832+lucasmarinskiba@users.noreply.github.com>
2025-06-21 16:30:54 -0300

    Update ResourceManager.h

--- include/ResourceManager.h
+++ include/ResourceManager.h
@@ -1,16 +1,20 @@
 #pragma once
-#include <SFML/Graphics.hpp>
+
 #include <SFML/Audio.hpp>
 #include <unordered_map>
+#include <map>
 #include <string>
+#include <SFML/Graphics.hpp>
 
 class ResourceManager {
 public:
+    static sf::Texture& getTexture(const std::string& filename);
     sf::Texture& loadTexture(const std::string& path);
     sf::Font& loadFont(const std::string& path);
     sf::SoundBuffer& loadSound(const std::string& path);
     void unloadAll();
 private:
+    static std::map<std::string, sf::Texture> textures;
     std::unordered_map<std::string, sf::Texture> textures;
     std::unordered_map<std::string, sf::Font> fonts;
     std::unordered_map<std::string, sf::SoundBuffer> sounds;
