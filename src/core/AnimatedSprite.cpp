78ca9d40b22a0346e4dabd01c0448ba1c3c7f1f8
Lucas Daniel Marín <116664832+lucasmarinskiba@users.noreply.github.com>
2025-06-21 17:52:30 -0300

    Update AnimatedSprite.cpp

--- src/core/AnimatedSprite.cpp
+++ src/core/AnimatedSprite.cpp
@@ -14,8 +14,8 @@ void AnimatedSprite::setTexture(const sf::Texture& texture, int fw, int fh, int 
     sprite.setTextureRect(sf::IntRect(0, 0, frameWidth, frameHeight));
 }
 
-void AnimatedSprite::setPosition(const sf::Vector2f& p) {
-    sprite.setPosition(p);
+void AnimatedSprite::setPosition(const sf::Vector2f& position) {
+    sprite.setPosition(position);
 }
 
 void AnimatedSprite::update(float dt) {
@@ -42,3 +42,16 @@ sf::Sprite& AnimatedSprite::getSprite() {
 void AnimatedSprite::move(sf::Vector2f offset) {
     sprite.move(offset);
 }
+
+void AnimatedSprite::setAnimation(const sf::Texture& texture, int fw, int fh, int fc, float fd) {
+    sprite.setTexture(texture);
+    frameWidth = fw;
+    frameHeight = fh;
+    frameCount = fc;
+    frameDuration = fd;
+    
+    // Configurar el primer frame
+    frameRect = {0, 0, frameWidth, frameHeight};
+    sprite.setTextureRect(frameRect);
+}
+
