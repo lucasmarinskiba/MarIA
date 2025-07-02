6107926f4fb84bda6deffcf318d13357a4a182de
Lucas Daniel Marín <116664832+lucasmarinskiba@users.noreply.github.com>
2025-07-02 12:23:46 -0300

    Update ResourceManager.h

--- include/ResourceManager.h
+++ include/ResourceManager.h
@@ -15,6 +15,7 @@ public:
     sf::SoundBuffer& loadSound(const std::string& path);
     void unloadAll();
 private:
+    std::map<std::string, sf::Texture> textures;
     std::unordered_map<std::string, sf::Texture> textures;
     std::unordered_map<std::string, sf::Font> fonts;
     std::unordered_map<std::string, sf::SoundBuffer> sounds;
